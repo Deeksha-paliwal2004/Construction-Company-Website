f2a9ea3b2d86170b1f1f06967cc3c63f37e08149
Deeksha-paliwal2004 <p16deeksha@gmail.com>
2025-07-17 13:13:48 +0530

    added project-detail page

--- scripts/projects.js
+++ scripts/projects.js
@@ -2,29 +2,33 @@ const extract = async ( ) => {
     let params = await fetch("../project_detail.json")
     let data = await params.json();
     data.forEach(item=> {
-        if(item.type == "Commercial"){
-            let div = document.createElement('div')
+       let div = document.createElement('div')
             div.classList.add("box")
             div.innerHTML = item.title
+             
             div.style.backgroundImage = `url(../${item.image})`
             div.style.backgroundSize = "cover"
             div.style.backgroundPosition = "center"
+        if(item.type == "Commercial"){
+             
           let pr =  document.getElementById("comm")
             pr.appendChild(div)
+            
         }
         else {
-             let div = document.createElement('div')
-            div.classList.add("box")
-            div.innerHTML = item.title
-            div.style.backgroundImage = `url(../${item.image})`
-            div.style.backgroundSize = "cover"
-            div.style.backgroundPosition = "center"
+             
           let pr =  document.getElementById("res")
             pr.appendChild(div)
+           
         }
-        
+        div.onclick = ()=>{
+          displaydeep(item) 
+        }
     });
     
 }
  extract();
-
+ function displaydeep(it){
+  const name = encodeURIComponent(it.title)
+      window.location.href =  `../indexes/project_info.html?name=${name}` 
+ }
