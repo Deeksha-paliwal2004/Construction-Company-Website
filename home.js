b481773ef562dd6b3b18a61bcbb117000022f7dc
Deeksha-paliwal2004 <p16deeksha@gmail.com>
2025-07-21 22:42:07 +0530

    deletion and modification performed

--- home.js
+++
@@ -1,9 +0,0 @@
-function scrollToSection(id) {
-  const section = document.getElementById(id);
-  section.scrollIntoView({ behavior: 'smooth' });
-}
-
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-  e.preventDefault();
-  alert('Message sent successfully!');
-});
